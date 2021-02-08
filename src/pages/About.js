df83e21c6dfa3f7482eae457aede8b5146920680
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-08 11:50:15 +0100

    add section and text

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero earum in aliquam deleniti ex dicta consequuntur eum saepe quibusdam minus mollitia totam assumenda, debitis ipsum quia! Id porro animi rerum.
+      Non, quibusdam molestiae! Assumenda, mollitia, numquam saepe id quia recusandae optio quis, perspiciatis obcaecati eum repellendus inventore quisquam corrupti doloremque expedita atque laudantium at. Labore quis consectetur aperiam atque laborum.
+      Magnam aperiam laudantium explicabo sed enim cum veniam aut illo voluptatum dignissimos aliquid iste optio, id totam? Quo, sit eveniet. Delectus, quae tempore. Alias magni amet quasi fuga, magnam voluptatum!</p>
+    </section>
   )
 }
 
